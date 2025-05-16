f90a49941302c2c84f1c6c6ea18d3963c4e2fed0
Eyidire <direhayees9120@gmail.com>
2025-05-16 08:50:41 -0700

    Fix PostCSS config and install Tailwind dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,14 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  css: {
+    postcss: path.resolve(__dirname, './postcss.config.js'), 
   },
 })
